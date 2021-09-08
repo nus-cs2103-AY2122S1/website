a504716772b8535cac2a461e5a86e4b6482f9798
damithc <damithc@github>
2021-09-08 17:40:16 +0800

    Site Update. [skip ci]

--- schedule/week12/admin.page-vue-render.js
+++ schedule/week12/admin.page-vue-render.js
@@ -20,6 +20,6 @@ with(this){return _c('span',{staticClass:"badge badge-secondary font-weight-norm
 with(this){return _c('ul',[_c('li',[_v("Submit the LumiNUS survey to declare that you have followed the proper procedure for reusing/adapting code from elsewhere.")])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 3.0.6")]),_v(", generated on Tue, 7 Sept 2021, 21:09:49 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 3.0.6")]),_v(", generated on Wed, 8 Sept 2021, 17:37:15 GMT+8]")])])])}
 }];
   
