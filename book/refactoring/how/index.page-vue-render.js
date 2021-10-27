2f697eb8762ae23be98928db4a92b67bef2465b6
Damith C. Rajapakse <damith@gmail.com>
2021-10-27 20:50:07 +0800

    Site Update. [skip ci]

--- book/refactoring/how/index.page-vue-render.js
+++ book/refactoring/how/index.page-vue-render.js
@@ -20,6 +20,6 @@ with(this){return _c('ol',[_c('li',[_c('a',{attrs:{"href":"https://refactoring.c
 with(this){return _c('div',[_c('div')])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 3.0.6")]),_v(", generated on Wed, 27 Oct 2021, 18:02:36 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 3.0.6")]),_v(", generated on Wed, 27 Oct 2021, 20:46:21 GMT+8]")])])])}
 }];
   
